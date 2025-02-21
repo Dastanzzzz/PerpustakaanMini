45fbba1105ae772de2705c9e1b68edb21aa8693f
Dastanzzzz <destantyo.browen@gmail.com>
2025-02-21 18:43:38 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(34, 96)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(125, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Tambah Buku"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(194, 96)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(125, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "Cari Buku"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(194, 152)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(125, 29)
+        Button3.TabIndex = 2
+        Button3.Text = "Tampilkan Buku"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(194, 205)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(125, 29)
+        Button4.TabIndex = 3
+        Button4.Text = "Keluar"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(34, 152)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(125, 29)
+        Button5.TabIndex = 4
+        Button5.Text = "Edit Buku"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(34, 205)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(125, 29)
+        Button6.TabIndex = 5
+        Button6.Text = "Hapus Buku"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(313, 78)
+        PictureBox1.TabIndex = 6
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(350, 279)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim judul As String = InputBox("Masukkan Judul Buku:", "Tambah Buku")
+        If judul <> "" Then
+            System.IO.File.AppendAllText("buku.txt", judul & vbCrLf)
+            MessageBox.Show("Buku berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If System.IO.File.Exists("buku.txt") Then
+            Dim daftarBuku As String = System.IO.File.ReadAllText("buku.txt")
+            MessageBox.Show("Daftar Buku:" & vbCrLf & daftarBuku, "Daftar Buku", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Belum ada buku dalam daftar.", "Daftar Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim keyword As String = InputBox("Masukkan kata kunci:", "Cari Buku")
+        Dim hasil As String = ""
+
+        If System.IO.File.Exists("buku.txt") Then
+            Dim buku() As String = System.IO.File.ReadAllLines("buku.txt")
+            For Each judul In buku
+                If judul.ToLower().Contains(keyword.ToLower()) Then
+                    hasil &= judul & vbCrLf
+                End If
+            Next
+        End If
+
+        If hasil <> "" Then
+            MessageBox.Show("Buku ditemukan:" & vbCrLf & hasil, "Hasil Pencarian", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Buku tidak ditemukan.", "Hasil Pencarian", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Memastikan file buku.txt ada
+        If Not System.IO.File.Exists("buku.txt") Then
+            MessageBox.Show("Belum ada buku dalam daftar.", "Edit Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Input buku yang ingin diedit
+        Dim judulLama As String = InputBox("Masukkan judul buku yang ingin diedit:", "Edit Buku")
+        Dim judulBaru As String = InputBox("Masukkan judul baru:", "Edit Buku")
+
+        If judulLama = "" OrElse judulBaru = "" Then
+            MessageBox.Show("Judul tidak boleh kosong!", "Edit Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Baca semua buku dari file
+        Dim buku As List(Of String) = System.IO.File.ReadAllLines("buku.txt").ToList()
+
+        ' Cek apakah buku ada dalam daftar
+        Dim index As Integer = buku.FindIndex(Function(b) b.ToLower() = judulLama.ToLower())
+        If index <> -1 Then
+            buku(index) = judulBaru
+            System.IO.File.WriteAllLines("buku.txt", buku)
+            MessageBox.Show("Buku berhasil diubah!", "Edit Buku", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Buku tidak ditemukan!", "Edit Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        ' Memastikan file buku.txt ada
+        If Not System.IO.File.Exists("buku.txt") Then
+            MessageBox.Show("Belum ada buku dalam daftar.", "Hapus Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Input judul buku yang ingin dihapus
+        Dim judulHapus As String = InputBox("Masukkan judul buku yang ingin dihapus:", "Hapus Buku")
+
+        If judulHapus = "" Then
+            MessageBox.Show("Judul tidak boleh kosong!", "Hapus Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Baca semua buku dari file
+        Dim buku As List(Of String) = System.IO.File.ReadAllLines("buku.txt").ToList()
+
+        ' Filter buku yang tidak dihapus
+        Dim bukuBaru As List(Of String) = buku.Where(Function(b) Not b.ToLower().Equals(judulHapus.ToLower())).ToList()
+
+        ' Jika jumlah buku tidak berubah, berarti buku tidak ditemukan
+        If buku.Count = bukuBaru.Count Then
+            MessageBox.Show("Buku tidak ditemukan!", "Hapus Buku", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            System.IO.File.WriteAllLines("buku.txt", bukuBaru)
+            MessageBox.Show("Buku berhasil dihapus!", "Hapus Buku", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+End Class
